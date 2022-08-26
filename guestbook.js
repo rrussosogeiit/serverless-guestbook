4c3667cd7daaf8bb656417647a4cc79791d16e57
rrussosogeiit <104077600+rrussosogeiit@users.noreply.github.com>
2022-08-26 11:44:37 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9fb713e2.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
